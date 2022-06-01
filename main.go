ed863c626e8f6c1dc2ca22818c033fb00abfc351
richard-on <richard-on@mail.ru>
2022-06-01 12:30:03 +0300

    Add docker compose

--- main.go
+++ main.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"log"
-	"queueBot/bot"
+	bot "queueBot/src/bot"
 )
 
 func main() {
-
+	log.Println("Initializing Database...")
 	err := bot.InitDb()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	log.Println("Creating Tables...")
 	err = bot.CreateTables()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	log.Println("Starting bot...")
 	bot.Bot()
 
 }
