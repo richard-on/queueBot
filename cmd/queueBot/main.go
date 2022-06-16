607353bb322cb34eeb7b313b5898f0c4a181ca21
richard-on <richard-on@mail.ru>
2022-06-16 21:22:40 +0300

    Change project structure

--- cmd/queueBot/main.go
+++ cmd/queueBot/main.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"log"
-	bot "queueBot/src/bot"
+	bot2 "queueBot/cmd/queueBot/bot"
 )
 
 func main() {
 	log.Println("Initializing Database...")
-	err := bot.InitDb()
+	err := bot2.InitDb()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.Println("Creating Tables...")
-	err = bot.CreateTables()
+	err = bot2.CreateTables()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.Println("Starting bot...")
-	bot.Bot()
+	bot2.Bot()
 
 }
